45265cfe669321a55cbad95827723fd00c7e2595
Kawsermony <71562296+Kawsermony@users.noreply.github.com>
2020-09-21 15:16:56 +0600

    New

---
+++ script.js
@@ -0,0 +1,247 @@
+/*var a,b,c;
+a=10;
+b=3;
+c=a+b;
+document.write(c);
+document.write("<br>");
+a++;
+c=a+b;
+document.write(c);
+const d=10;
+d= 15;
+document.write(d);
+*/
+/*var x="hello";
+x= 23;
+x=true;
+x=["html","css","js"];
+x={
+name: "obaid",
+id: "kak"
+};
+x=undefined;
+x;
+x=null;
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
+*/
+
+/*var x=10;
+var y=3;
+x%=y;
+document.write(x);
+*/
+/*document.write(4=="4");
+if(10>10){
+document.write("greater")
+}else{
+document.write("lower")
+}
+*/
+let a, b, c;
+a=10;
+b=5;
+
+c= a-b;
+document.write(c);
+
+var e,f,g;
+e=10;
+f=5;
+var e=7;
+c= e-f;
+document.write(c);
+
+const aa=10;
+const bb=5;
+ const cc= aa-bb;
+ document.write("<br>")
+ document.write(cc);
+
+ var x;
+ document.write(x);
+ document.write("<br>");
+ document.write(typeof x);
+ var x=10;
+ var y=2;
+ x**=y; // x=x**y
+ document.write(x);
+ var x = "30";
+ document.write(typeof x);
+
+document.write("<br>");
+
+ ac=10;
+ ab=20;
+ console.log(ac<=ab); 
+
+ document.write("<br>");
+ var af= 10;
+ var bf= 20;
+if(a>b){
+document.write("bf is greater");
+}else{
+document.write("af is not greater than bf")
+}
+
+document.write("<br>");
+var af= 10;
+ var bf= 20;
+if(a>b){
+document.write("bf is greater");
+}else{
+document.write("af is lower")
+}
+document.write("<br>");
+var per = 57;
+if(per >=80 && per <=100){
+document.write("A+")
+}else if(per >=60 && per <80)
+{
+document.write("A")
+}else if(per >=45 && per <60){
+document.write("A-")
+ }else if(per >=33 && per <45){
+document.write("B")
+ }else if(per <33){
+ document.write("fail")
+}else{
+document.write("please enter valid number");
+}
+
+document.write("<br>");
+var day = 6;
+switch (day) {
+case 1:
+document.write("today is sunday");
+break;
+case 2:
+document.write("today is monday");
+break;
+case 3:
+document.write("today is tuesday");
+break;
+case 4:
+document.write("today is wednesday");
+break;
+case 5:
+document.write("today is Thursday");
+break;
+case 6:
+document.write("today is friday");
+break;
+case 7:
+document.write("today is saturday");
+break;
+default:
+document.write("enterr the valid date ");
+};
+document.write("<br>");
+var age= 15;
+switch (false) {
+case (age >14 && age <= 20):
+document.write("you are eligble");
+break;
+case (age >20 && age <= 30):
+default:
+document.write("enter the valid date")
+}
+
+document.write("<br>");
+var ad=20;
+if(ad>30){
+document.write("true");
+}else{
+document.write("false");
+}
+document.write("<br>");
+
+var ar= 10;
+var br= 20;
+if (ar>br) {
+alert ("ar is greater");
+}else{
+alert ("br is greater");
+}
+
+document.write("<br>");
+var m = prompt("what is your name ?");
+console.log(m);
+function hello() {
+document.write("kawser mony");
+document.write("<br>");
+}
+
+
+var per = prompt("enter percentage");
+if(per >=80 && per <=100){
+document.write("A+")
+}else if(per >=60 && per <80)
+{
+document.write("A")
+}else if(per >=45 && per <60){
+document.write("A-")
+ }else if(per >=33 && per <45){
+document.write("B")
+ }else if(per <33){
+ document.write("fail")
+}else{
+document.write("please enter valid number");
+}
+
+function hello() {
+document.write("hello EveryOne")
+}
+
+
+document.write("<br>");
+var per = prompt("enter percentage");
+if(per >=80 && per <=100){
+document.write("A+")
+}else if(per >=60 && per <80)
+{
+document.write("A")
+}else if(per >=45 && per <60){
+document.write("A-")
+ }else if(per >=33 && per <45){
+document.write("B")
+ }else if(per <33){
+ document.write("fail")
+}else{
+document.write("please enter valid number");
+}
+
+function hello() {
+document.write("hello world");
+document.write("<br>");
+}
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+function sum(as,bs,cs) {
+document.write(as-bs+cs);
+document.write("<br>");
+}
+sum(20,10,30);
+
+function sum() {
+var as=10;
+document.write(a);
+document.write("<br>");
+}
+
+
+
+
+
+
+
+
+
